bce632acdc4bf24e951dc3944db1e4e2eee5fe53
Crumbtray <clint.isaac.wong@gmail.com>
2014-01-19 18:19:12 -0700

    Submission-Ready

--- server.py
+++ server.py
@@ -31,8 +31,49 @@ class MyWebServer(SocketServer.BaseRequestHandler):
     
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall("OK")
+        words = self.data.split()
+        url = words[1]
+        response = self.parseUrl(url)
+        self.request.sendall(response)
+
+    # Parses the Url and returns the HTTP Response (either 200 OK, or 404 Not Found)
+    def parseUrl(self, url):
+        urlWords = url.split("/")
+        for word in urlWords:
+            if word == "..":
+                url = "youtryingtohackme?"
+
+        # If there is no file requested (".") then we are looking for index.html of the directory
+        if (urlWords[-1].find(".") == -1):
+            if urlWords[-1]:
+                url = url + "/"
+            url = url + "index.html"
+        relative_path = "www" + url
+
+        contentType = relative_path.split(".")[-1]
+
+        content_type_string = "Content-Type: text/"+ contentType +"; charset=UTF-8\r\n"
+
+        try:
+            with open(relative_path, 'r') as server_file:
+                content = server_file.read()
+            content_length = len(content)
+            http_header = "HTTP/1.1 200 OK\r\n"
+        except IOError:
+            http_header = "HTTP/1.1 404 Not Found\r\n"
+            with open("404.html", 'r') as not_found_file:
+                content = not_found_file.read()
+            content_length = len(content)
+
+        # Here we build the HTTP Header:
+        
+        x_content_words = "X-Content-Type-Options: nosniff\r\n" 
+        content_length_string = "Content-Length: " + str(content_length) + "\r\n"
+        server_string = "Server: HTTP server\r\n"
+        connection_string = "Connection: close\r\n"
+
+        http_response = http_header + content_type_string + x_content_words + content_length_string + server_string + connection_string + "\r\n" + content
+        return http_response
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
